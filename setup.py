1a99ec75ab4528e3373afaebe35b0e42570b8ddb
mkolopanis <matthew.kolopanis@gmail.com>
2017-06-14 12:57:13 -0700

    setup adds tests and data

--- setup.py
+++ setup.py
@@ -12,9 +12,10 @@ setup_args = {
     'description': 'an utility to predict the theoretical wedge leakage from a radio intereferometric array',
     'package_dir': {'py21cmwedge': 'py21cmwedge'},
     'packages': ['py21cmwedge'],
-    'version': '__version__',
-    'package_data': {'py21cmwedge': [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/data', f))]},
-    'install_requires': ['numpy>1.10', 'scipy', 'astropy>1.2', 'nose']
+    'version': __version__,
+    'package_data': {'py21cmwedge': [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/data', f))] + [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/tests/data', f))] },
+    'install_requires': ['numpy>1.10', 'scipy', 'astropy>1.2', 'nose'],
+    'test_suite': 'py21cmwedge'
 }
 
 if __name__ == '__main__':
