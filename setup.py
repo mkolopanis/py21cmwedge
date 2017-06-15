0cd550363a7a719ff9f667e884e4fdc2f0934c6b
mkolopanis <matthew.kolopanis@gmail.com>
2017-06-15 11:29:27 -0700

    pep8 in setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup_args = {
     'package_dir': {'py21cmwedge': 'py21cmwedge'},
     'packages': ['py21cmwedge'],
     'version': __version__,
-    'package_data': {'py21cmwedge': [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/data', f))] + [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/tests/data', f))] },
+    'package_data': {'py21cmwedge': [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/data', f))] + [f for f in listdir('./py21cmwedge/data') if op.isfile(op.join('./py21cmwedge/tests/data', f))]},
     'install_requires': ['numpy>1.10', 'scipy', 'astropy>1.2', 'nose'],
     'test_suite': 'py21cmwedge'
 }
