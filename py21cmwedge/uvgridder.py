fec1899f36d5b92d7fee057487a47e5debc3c39f
mkolopanis <matthew.kolopanis@gmail.com>
2017-07-19 14:42:51 -0700

    add 5 pixels on either side of the longest baseline. This will help for arrays whose longest baselines are exactly EW

--- py21cmwedge/uvgridder.py
+++ py21cmwedge/uvgridder.py
@@ -229,7 +229,7 @@ class UVGridder(object):
         """Create UV coverage from object data."""
         self.uv_size = int(np.round(self.bl_len_max
                                     / self.wavelength
-                                    / self.uv_delta).max() * 1.1) * 2 + 1
+                                    / self.uv_delta).max()) * 2 + 5
         self.uvf_cube = np.zeros(
             (self.freqs.size, self.uv_size, self.uv_size))
         for uv_key in self.uvbins.keys():
