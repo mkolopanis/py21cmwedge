ceb00aacab5aad23b090d3b29489c5448de6f92e
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2017-09-20 14:37:12 -0700

    renamed E_inv to Ez function. integration of 1./E(z) now requires lmabda function call

--- py21cmwedge/cosmo.py
+++ py21cmwedge/cosmo.py
@@ -4,18 +4,18 @@ from scipy.integrate import quad
 
 
 # Refer to (Liu et al 2014a) Appendix A for formula references
-def E_inv(redshift=None, omega_matter=None,
-          omega_lambda=None, omega_curv=None):
+def Ez(redshift=None, omega_matter=None,
+       omega_lambda=None, omega_curv=None):
     """Compute co-moving distance by integrating this function."""
-    return 1. / np.sqrt(omega_matter * (1. + redshift)**3
-                        + omega_curv * (1. + redshift)**2 + omega_lambda)
+    return np.sqrt(omega_matter * (1. + redshift)**3
+                   + omega_curv * (1. + redshift)**2 + omega_lambda)
 
 
 def comoving_distance(redshift=None, hubble_param=1, omega_matter=.27,
                       omega_lambda=.73, omega_curv=0.0):
         """Calculate comoving distance for a Lambda_CDM cosmology."""
         hubble_dist = 3e3 / hubble_param
-        integral, err = quad(E_inv, 0., redshift,
+        integral, err = quad(lambda z: 1/E(z), 0., redshift,
                              args=(omega_matter, omega_lambda, omega_curv))
         return hubble_dist * integral
 
