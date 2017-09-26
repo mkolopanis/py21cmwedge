f1a334b50e1b8f7d328d680cc89b8951ad38b6c6
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2017-09-26 11:45:01 -0700

    updated tests for cosmo.py

--- py21cmwedge/tests/test_cosmo.py
+++ py21cmwedge/tests/test_cosmo.py
@@ -9,26 +9,19 @@ from py21cmwedge import cosmo
 testdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
 
-def test_u_to_k():
-    """Test |U| to k against a known value."""
-    test_u = 1.5
-    redshift = 8.5
-    u_to_k = cosmo.u2kperp(test_u, redshift)
-    nt.assert_equal(u_to_k, 0.0014275626347673188)
-
-
-def test_k_to_u():
-    """Test k to |u| against known value."""
-    test_k = 0.0014275626347673188
-    redshift = 8.5
-    k_to_u = cosmo.kperp2u(test_k, redshift)
-    nt.assert_equal(k_to_u, 1.5)
-
-
 def test_u_to_k_to_u():
     """Convert a |U| value to k and back again."""
     test_u = 1.5
     redshift = 8.5
     u_to_k = cosmo.u2kperp(test_u, redshift)
     u_to_k_to_u = cosmo.kperp2u(u_to_k, redshift)
-    nt.assert_equal(test_u, u_to_k_to_u)
+    nt.assert_true(np.isclose(test_u, u_to_k_to_u))
+
+
+def test_eta_to_kpar_to_u():
+    """Convert a delay, eta, to k_par and back."""
+    test_eta = 1./50e6  # 50MHz test bandwidth
+    redshift = 8.5
+    eta_to_kpar = cosmo.eta2kpar(test_eta, redshift)
+    eta_to_kpar_to_eta = cosmo.kpar2eta(eta_to_kpar, redshift)
+    nt.assert_true(np.isclose(test_eta, eta_to_kpar_to_eta))
