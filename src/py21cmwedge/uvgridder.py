fix: update a rotation in nearest mode

src/py21cmwedge/uvgridder.py
@@ -297,18 +297,20 @@ class UVGridder(object):
                 _range = np.arange(self.uv_size) - (self.uv_size - 1) / 2.0
                 _range *= self.uv_delta
                 x, y = _range, _range
-                x.shape += (1,)
-                y.shape += (1,)
+                x = np.expand_dims(x, -1)
+                y = np.expand_dims(y, -1)
                 x = u - x
                 y = v - y
-                u_index = np.argmin(np.abs(x), axis=0).squeeze()
-                v_index = np.argmin(np.abs(y), axis=0).squeeze()
+
+                u_index = np.argmin(np.abs(x), axis=0)
+                v_index = np.argmin(np.abs(y), axis=0)
 
                 weights = np.zeros(
-                    (x.shape[-1], self.uv_size, self.uv_size), dtype=complex
+                    (self.freqs.size, self.uv_size, self.uv_size), dtype=np.float64
                 )
+                # v,u indexing because y is the outer dimension in memory
+                weights[range(self.freqs.size), v_index, u_index] += 1.0
 
-                weights[range(weights.shape[0]), u_index, v_index] = 1.0
             case _:
                 raise ValueError(
                     f"Unknown value for 'spatial_function': {spatial_function}"

tests/test_uvgridder.py
@@ -354,7 +354,7 @@ def test_grid_uv_deltas():
     test_obj.uvw_to_dict()
     test_obj.grid_uvw(convolve_beam=False, spatial_function="nearest")
 
-    np.testing.assert_allclose(test_obj.uvf_cube[0, 33, 18], 10)
+    np.testing.assert_allclose(test_obj.uvf_cube[0, 18, 33], 10)
 
 
 def test_grid_uv_deltas_multi_freq():
@@ -367,10 +367,10 @@ def test_grid_uv_deltas_multi_freq():
     test_obj.uvw_to_dict()
     test_obj.grid_uvw(convolve_beam=False, spatial_function="nearest")
 
-    np.testing.assert_allclose(test_obj.uvf_cube[1, 33, 18], 10)
-    np.testing.assert_allclose(test_obj.uvf_cube[0, 23, 18], 10)
-    np.testing.assert_allclose(test_obj.uvf_cube[1, 23, 18], 0)
-    np.testing.assert_allclose(test_obj.uvf_cube[0, 33, 18], 0)
+    np.testing.assert_allclose(test_obj.uvf_cube[1, 18, 33], 10)
+    np.testing.assert_allclose(test_obj.uvf_cube[0, 18, 23], 10)
+    np.testing.assert_allclose(test_obj.uvf_cube[1, 18, 23], 0)
+    np.testing.assert_allclose(test_obj.uvf_cube[0, 18, 33], 0)
 
 
 def test_grid_uv_error():